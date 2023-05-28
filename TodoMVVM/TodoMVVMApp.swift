07444efbb998f194ace19bb0967304018232bf62
Said-Nasir Ismailov <ismailovsn12@gmail.com>
2023-05-28 20:40:35 +0900

    Initial Commit

---
+++ TodoMVVM/TodoMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoMVVMApp.swift
+//  TodoMVVM
+//
+//  Created by Саид-Насир Исмаилов on 2023/05/28.
+//
+
+import SwiftUI
+
+@main
+struct TodoMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
