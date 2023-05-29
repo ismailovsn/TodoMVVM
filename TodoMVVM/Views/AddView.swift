5e7083a58f411fc9acead93d7e7dc6b6ebdc3a47
Said-Nasir Ismailov <ismailovsn12@gmail.com>
2023-05-29 18:08:45 +0900

    feat: add func for adding new todo list items

--- TodoMVVM/Views/AddView.swift
+++ TodoMVVM/Views/AddView.swift
@@ -8,7 +8,12 @@
 import SwiftUI
 
 struct AddView: View {
+    @Environment(\.presentationMode) var presentationMode
+    @EnvironmentObject var listViewModel: ListViewModel
     @State private var textFieldText: String = ""
+    
+    @State var alertTitle: String = ""
+    @State var showAlert: Bool = false
     
     var body: some View {
 //        ScrollView {
@@ -16,15 +21,36 @@ struct AddView: View {
                 TextField("Type something here...", text: $textFieldText)
                 
                 Button {
-                    
+                    saveButtonPressed()
                 } label: {
                     Text("Save")
-                }
+                }.foregroundColor(textIsAppropriate() ? .blue : .red)
 
 //            }
                 
         }
         .navigationTitle("Add an Item ✏️")
+        .alert(isPresented: $showAlert, content: getAlert)
+    }
+    
+    func saveButtonPressed() {
+        if textIsAppropriate() {
+            listViewModel.addItem(title: textFieldText)
+            presentationMode.wrappedValue.dismiss()
+        }
+    }
+    
+    func textIsAppropriate() -> Bool {
+        if textFieldText.count < 3 {
+            alertTitle = "Your new todo item must be at least 3 characters long"
+            showAlert.toggle()
+            return false
+        }
+        return true
+    }
+    
+    func getAlert() -> Alert {
+        return Alert(title: Text(alertTitle))
     }
 }
 
@@ -33,5 +59,6 @@ struct AddView_Previews: PreviewProvider {
         NavigationView {
             AddView()
         }
+        .environmentObject(ListViewModel())
     }
 }
