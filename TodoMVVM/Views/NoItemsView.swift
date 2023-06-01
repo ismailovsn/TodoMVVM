4853cfd23577aa308f3515e4182bef4c032e1ada
Said-Nasir Ismailov <ismailovsn12@gmail.com>
2023-06-01 21:47:56 +0900

    feat: it there is no item in list, show corresponding text

---
+++ TodoMVVM/Views/NoItemsView.swift
@@ -0,0 +1,20 @@
+//
+//  NoItemsView.swift
+//  TodoMVVM
+//
+//  Created by Саид-Насир Исмаилов on 2023/06/01.
+//
+
+import SwiftUI
+
+struct NoItemsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NoItemsView_Previews: PreviewProvider {
+    static var previews: some View {
+        NoItemsView()
+    }
+}
