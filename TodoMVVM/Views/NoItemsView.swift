600201ff10148aa2f9878b58cbf44effba242848
Said-Nasir Ismailov <ismailovsn12@gmail.com>
2023-06-02 07:47:53 +0900

    feat: add view for showing it when there are no items in list and add animation

--- TodoMVVM/Views/NoItemsView.swift
+++ TodoMVVM/Views/NoItemsView.swift
@@ -8,13 +8,62 @@
 import SwiftUI
 
 struct NoItemsView: View {
+    
+    @State var animate: Bool = false
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack(spacing: 10) {
+                Text("There are no items!")
+                    .font(.title)
+                    .fontWeight(.semibold)
+                Text("Press the add button and add a bunch of items to your todo list!")
+                    .padding(.bottom)
+                
+                NavigationLink(
+                    destination: AddView(),
+                    label: {
+                    Text("Add Something ⚡️")
+                        .foregroundColor(.white)
+                        .font(.headline)
+                        .frame(height: 55)
+                        .frame(maxWidth: .infinity)
+                        .background(animate ? Color.red : Color.accentColor)
+                        .cornerRadius(10)                    
+                })
+                .padding(.horizontal, animate ? 30 : 50)
+                .shadow(
+                    color: animate ? Color.red.opacity(0.7) : Color.accentColor.opacity(0.7),
+                    radius: animate ? 30 : 10,
+                    x: 0,
+                    y: animate ? 30: 10)
+            }
+            .multilineTextAlignment(.center)
+            .padding(40)
+            .onAppear(perform: addAnimation)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+        
+    func addAnimation() {
+        guard !animate else { return }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+            withAnimation(
+                Animation
+                    .easeInOut(duration: 2.0)
+                    .repeatForever()
+            ) {
+                animate.toggle()
+            }
+        }
     }
 }
 
 struct NoItemsView_Previews: PreviewProvider {
     static var previews: some View {
-        NoItemsView()
+        NavigationView {
+            NoItemsView()
+                .navigationTitle("Title")
+        }
     }
 }
